59fb766a2d385f72e6b9f366ea190144f5ee59f3
Akansha <akanshasingh16806@gmail.com>
2026-06-10 14:10:20 +0530

    day8 assignment is uploaded

---
+++ day8/q31.cpp
@@ -0,0 +1,18 @@
+//wap to print character triangle 
+#include<iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << char(64 + j) << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ day8/q32.cpp
@@ -0,0 +1,18 @@
+//wap to print repeated number pattern
+#include<iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
